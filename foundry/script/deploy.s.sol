0029371e7ec62b883d243a2e86d2f55798e290de
aperisss <peris.adam@outlook.fr>
2024-08-26 15:32:59 +0000

    foundry

--- foundry/script/deploy.s.sol
+++ foundry/script/deploy.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {Script} from "../../foundrylib/lib/forge-std/src/Script.sol";
 import {SimpleStorage} from "../src/simpleStorage.sol";
