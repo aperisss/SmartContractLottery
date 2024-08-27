cbbb8733c70f7322e090c2a32b11d038bf1ae8d1
Adam PERIS <aperis@made-f0Cr8s5.clusters.42paris.fr>
2024-08-27 15:45:26 +0200

    test .sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
 source /root/.bashrc
 foundryup
-mkdir foundrylib
-cd foundrylib
-forge init 
-rm -rf script src test
+forge init foundrylib
+rm -rf foundrylib/script foundrylib/src foundrylib/test
 cd ..
+
+
+
+
+
+
+# source /root/.bashrc
+# foundryup
+# mkdir foundrylib
+# cd foundrylib
+# forge init 
+# rm -rf script src test
+# cd ..
