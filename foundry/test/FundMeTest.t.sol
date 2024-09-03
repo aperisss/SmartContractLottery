731ffcc415254474ec8652d422552f5531febee1
aperisss <peris.adam@outlook.fr>
2024-09-03 12:19:27 +0000

    push

--- foundry/test/FundMeTest.t.sol
+++ foundry/test/FundMeTest.t.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe =  new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -19,5 +22,10 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public {
         assertEq(fundMe.i_owner(), msg.sender);
 
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
